74fc10f3d24698da6df0f207bc0375d452b5242e
jkgrad <ojk@kaist.ac.kr>
2021-06-02 15:08:28 +0900

    Removed unnecessary codes.

--- code/crowdsourcing/knn.py
+++
@@ -1,7 +0,0 @@
-for ex, lb in zip(features, labels):
-        sim = torch.mm(ex.unsqueeze(0), train_features)
-        nbr, ind = sim.topk(k=K, dim=-1)
-        nbr_lab  = torch.gather(train_labels.expand(ex.shape[0], -1), index=ind, dim=-1)
-        unique, lab_cnt  = nbr_lab.unique(return_counts=True)
-        if lb == (unique[torch.argmax(lab_cnt)]):
-            num_correct += 1
